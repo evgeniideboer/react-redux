7f33c5a812e2b39cbe4bfb409ad3311a88baa499
Evgenii de-Bur <evd8@inbox.ru>
2023-08-04 12:51:41 +0300

    4. Action creators, arrays, refactoring

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import Test from "./test";
+import { addCustomAction, removeCustomAction } from "./store/customerReducer";
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector((state) => state.cash.cash);
-  console.log(cash);
+  const customers = useSelector((state) => state.customers.customers);
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash });
@@ -14,6 +15,18 @@ function App() {
 
   const getCash = (cash) => {
     dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomAction(customer.id));
   };
 
   return (
@@ -26,9 +39,39 @@ function App() {
         <button onClick={() => getCash(Number(prompt()))}>
           Снять со счёта
         </button>
-        <div style={{ fontSize: "3rem" }}>
-          <Test />
+
+        <div>
+          <button onClick={() => addCustomer(prompt())}>
+            Добавлять клиента
+          </button>
         </div>
+        <div>
+          {customers.length > 0 ? (
+            <div>
+              {customers.map((customer) => (
+                <div
+                  onClick={() => removeCustomer(customer)}
+                  style={{
+                    fontSize: "2rem",
+                    border: "1px solid black",
+                    padding: "10px",
+                    marginTop: 5,
+                  }}
+                >
+                  {customer.name}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div style={{ fontSize: "3rem", marginTop: 20 }}>
+              Клиенты отсутствуют!
+            </div>
+          )}
+        </div>
+
+        {/* <div style={{ fontSize: "3rem" }}>
+          <Test />
+        </div> */}
       </div>
     </div>
   );
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,14 +2,31 @@ const defaultState = {
   customers: [],
 };
 
+const ADD_CUSTOMER = "ADD_CUSTOMER";
+const REMOVE_CUSTOMERS = "REMOVE_CUSTOMERS";
+
 export const customerReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_CUSTOMER":
-      return { ...state, cash: state.cash + action.payload };
-    case "GET_CUSTOMERS":
-      return { ...state, cash: state.cash - action.payload };
+    case ADD_CUSTOMER:
+      return { ...state, customers: [...state.customers, action.payload] };
+    case REMOVE_CUSTOMERS:
+      return {
+        ...state,
+        customers: state.customers.filter(
+          (customer) => customer.id !== action.payload
+        ),
+      };
 
     default:
       return state;
   }
 };
+
+export const addCustomAction = (payload) => ({
+  type: ADD_CUSTOMER,
+  payload,
+});
+export const removeCustomAction = (payload) => ({
+  type: REMOVE_CUSTOMERS,
+  payload,
+});
